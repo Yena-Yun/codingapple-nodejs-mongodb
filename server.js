fe3aee0a7387854e6959fcec90d51c2e165e2c66
Yena-Yun <cheryleduart@gmail.com>
2022-04-10 11:41:44 +0900

    feat: write 페이지 생성, form의 입력값을 POST로 서버에 보내기

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(8080, function () {
-  console.log("listening on 8080");
+  console.log('listening on 8080');
 });
 
-app.get("/pet", function (req, res) {
-  res.send("펫용품 쇼핑가능한 페이지입니다.");
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get("/beauty", function (req, res) {
-  res.send("뷰티용품 쇼핑가능한 페이지입니다.");
+app.get('/write', function (req, res) {
+  res.sendFile(__dirname + '/write.html');
 });
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.post('/add', function (req, res) {
+  res.send('전송완료');
+  console.log(req.body.title);
+  console.log(req.body.date);
+  console.log(req.body);
 });
